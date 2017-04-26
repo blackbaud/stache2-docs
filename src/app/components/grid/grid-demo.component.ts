34bfe31e8f3aaa6bc15f51a3233f58d2a71c5dc2
blackbaud-brandonhare <brandon.hare@blackbaud.com>
2017-04-26 09:30:39 -0400

    Updated grid.

--- src/app/components/grid/grid-demo.component.ts
+++ src/app/components/grid/grid-demo.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'stache-grid-demo',
-  templateUrl: './grid-demo.component.html'
+  templateUrl: './grid-demo.component.html',
+  styleUrls: ['./grid-demo.component.scss']
 })
 export class StacheGridDemoComponent implements OnInit {
   public ngOnInit(): void { }
