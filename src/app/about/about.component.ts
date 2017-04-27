0c54ca35645de2b50743b995250bbcf956feffc0
blackbaud-brandonhare <brandon.hare@blackbaud.com>
2017-04-27 12:01:32 -0400

    Starting learn content.

--- src/app/about/about.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'my-about',
-  templateUrl: './about.component.html'
-})
-export class AboutComponent {
-  constructor() {}
-}
