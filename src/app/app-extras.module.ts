995a7cc580719b40ac0d9dfb08e7cc892171ed24
Brandon Hare <blackbaud-brandonhare@users.noreply.github.com>
2017-05-12 16:40:41 -0400

    Updating release (#14)
    * Added installation instructions.
    
    * added correct folder structure for includes, cleaned up language.
    
    * Updated dependencies for release.
    
    * Update package.json
    
    * Moved includes in stache directory.
    
    * Adding folder because reasons.
    
    * Changes to docs (#15)
    
    * Updated tabs and spaces
    
    * Updated getting started

--- src/app/app-extras.module.ts
+++ src/app/app-extras.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 
-import { StacheModule } from '@blackbaud/stache';
+import { SkyAppConfig } from '@blackbaud/skyux-builder/runtime';
+import { StacheModule, StacheConfigService } from '@blackbaud/stache';
 
 import { StacheDemoComponentService } from './components/demo-component.service';
 
@@ -15,6 +16,10 @@ require('style-loader!./app.scss');
     StacheModule
   ],
   providers: [
+    {
+      provide: StacheConfigService,
+      useExisting: SkyAppConfig
+    },
     StacheDemoComponentService
   ],
   entryComponents: []
