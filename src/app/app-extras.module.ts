8adbef1b5f66270868427e3b37e85c4a24793c18
bdjones2410 <brandon.jones@blackbaud.com>
2017-06-15 13:15:16 -0400

    updated to latest stache plugins and version

--- src/app/app-extras.module.ts
+++ src/app/app-extras.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 
-import { SkyAppConfig } from '@blackbaud/skyux-builder/runtime';
-import { StacheModule, StacheConfigService } from '@blackbaud/stache';
+import { StacheModule } from '@blackbaud/stache';
 
 import { StacheDemoComponentService } from './components/demo-component.service';
 
@@ -16,10 +15,6 @@ require('style-loader!./app.scss');
     StacheModule
   ],
   providers: [
-    {
-      provide: StacheConfigService,
-      useExisting: SkyAppConfig
-    },
     StacheDemoComponentService
   ],
   entryComponents: []
