61a28126bec194cf44c5a605283398146e8f78d8
Lindsey Rix <lindsey.rix@blackbaud.com>
2017-06-23 09:57:31 -0400

    add home page actions (#37)
    * add home page actions and a new hero imgae
    
    * Update index.html
    
    * Delete hero-option-1.png
    
    * Delete hero-option-4.jpg
    
    * Delete hero-option-3.jpg
    
    * Delete hero-option-2.png

--- src/app/app-extras.module.ts
+++ src/app/app-extras.module.ts
@@ -1,10 +1,10 @@
+require('style-loader!./app.scss');
+
 import { NgModule } from '@angular/core';
 
 import { StacheModule } from '@blackbaud/stache';
 
 import { StacheDemoComponentService } from './components/demo-component.service';
-
-require('style-loader!./app.scss');
 
 // Specify entry components, module-level providers, etc. here.
 @NgModule({
