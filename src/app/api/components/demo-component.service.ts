2fdb67e54d113c40bf2d6f08c17dbbdc79914722
Lindsey Rix <lindsey.rix@blackbaud.com>
2017-07-06 14:32:54 -0400

    attempt to fix error

--- src/app/api/components/demo-component.service.ts
+++ src/app/api/components/demo-component.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-import { StacheDemoComponent } from './api/components/demo-component';
+import { StacheDemoComponent } from './demo-component';
 
 @Injectable()
 export class StacheDemoComponentService {
