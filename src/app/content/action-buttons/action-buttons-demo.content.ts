736266a5f31ba4b31b379505847385996abc5111
Lindsey Rix <lindsey.rix@blackbaud.com>
2017-07-07 10:12:16 -0400

    New navigation (#45)
    * Added installation instructions.
    
    * added correct folder structure for includes, cleaned up language.
    
    * nav changes
    
    * new navigation for site to serve 2 audiences
    
    * Fixed lint errors.
    
    * fix error
    
    * attempt to fix error
    
    * Added nvmrc

--- src/app/content/action-buttons/action-buttons-demo.content.ts
+++ src/app/content/action-buttons/action-buttons-demo.content.ts
@@ -1,20 +1,20 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'stache-action-buttons-demo',
-  templateUrl: 'action-buttons-demo.component.html'
+  selector: 'stache-action-buttons-content',
+  templateUrl: 'action-buttons-demo.content.html'
 })
-export class StacheActionButtonsDemoComponent {
+export class StacheActionButtonsDemoContent {
   public routes = [
     {
       name: 'Layout',
-      path: '/components/layout',
+      path: '/content/layout',
       icon: 'map-o',
       summary: 'Basic page layouts using Stache and skyux pattern, for quick page layouts.'
     },
     {
       name: 'Layout: Sidebar',
-      path: '/components/layout-sidebar',
+      path: '/content/layout-sidebar',
       icon: 'columns',
       summary: 'Side bar navigation layout pattern for creating quick documentation.'
     }
