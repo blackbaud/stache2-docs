1586b2e48710dcad4c884433d231c504b06d7e89
blackbaud-brandonhare <brandon.hare@blackbaud.com>
2017-07-17 13:58:04 -0400

    Added search to component collections.

--- src/app/api/components/demo-components.component.ts
+++ src/app/api/components/demo-components.component.ts
@@ -4,24 +4,46 @@ import { StacheDemoComponentService } from './demo-component.service';
 
 @Component({
   selector: 'stache-demo-components',
-  templateUrl: './demo-components.component.html'
+  templateUrl: './demo-components.component.html',
+  styleUrls: ['./demo-components.component.scss']
 })
 export class StacheDemoComponentsComponent implements OnInit {
   public routes: any[] = [];
+  private components: any[];
 
   public constructor(
     private componentService: StacheDemoComponentService) { }
 
   public ngOnInit(): void {
-    let components = this.componentService.getAllSortedByCollection('components');
-
-    components.forEach(component => {
-      this.routes.push({
-        path: [component.route],
-        name: component.name,
-        icon: component.icon,
-        summary: component.summary
-      });
+    this.components = this.componentService
+      .getAllSortedByCollection('components')
+      .map(component => {
+        return {
+          path: [component.route],
+          name: component.name,
+          icon: component.icon,
+          summary: component.summary
+        };
     });
+
+    this.routes = this.components;
+  }
+
+  public searchApplied(searchText: string) {
+    let filteredItems = this.components;
+    if (searchText) {
+      filteredItems = this.components.filter(function (item: any){
+        let property: any;
+        for (property in item) {
+          if (item.hasOwnProperty(property) && (property === 'name' || property === 'summary')) {
+            if (item[property].toLowerCase().indexOf(searchText.toLowerCase()) > -1) {
+              return true;
+            }
+          }
+        }
+        return false;
+      });
+    }
+    this.routes = filteredItems;
   }
 }
