0e29f70fbf2ad89a45cb0389ff6ce35ec480d7e1
Lindsey Rix <lindsey.rix@blackbaud.com>
2018-10-08 16:15:01 -0400

    remove sky temp pages

--- .skypagestmp/src/main-internal.aot.ts
+++
@@ -1,8 +0,0 @@
-import { platformBrowser } from '@angular/platform-browser';
-import { AppModuleNgFactory } from './ngfactory/app/app.module.ngfactory';
-
-import { SkyAppBootstrapper } from '@blackbaud/skyux-builder/runtime/bootstrapper';
-
-SkyAppBootstrapper.processBootstrapConfig().then(() => {
-  platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
-});
--- .skypagestmp/src/main.ts
+++
@@ -1,1 +0,0 @@
-// This can be overridden by the SPA to bootstrap things like the omnibar or auth.
