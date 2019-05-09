f6a2de7f3ec6387ca4e24bc605ee796f0a96f612
bdjones2410 <brandon.jones@blackbaud.com>
2019-05-09 13:33:08 -0400

    added warnings and replaced deprecated components

--- src/app/app-extras.module.ts
+++ src/app/app-extras.module.ts
@@ -6,12 +6,21 @@ import { StacheModule } from '@blackbaud/stache';
 
 import { StacheDemoContentService } from './content/demo-content.service';
 
+import {
+  SkyHeroModule,
+  SkyImageModule,
+  SkyVideoModule
+} from '@blackbaud/skyux-lib-media';
+
 // Specify entry components, module-level providers, etc. here.
 @NgModule({
   imports: [
     StacheModule
   ],
   exports: [
+    SkyHeroModule,
+    SkyImageModule,
+    SkyVideoModule,
     StacheModule
   ],
   providers: [
